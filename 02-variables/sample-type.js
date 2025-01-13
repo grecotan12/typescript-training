f2958573cec6f7018006f31138a0bb1351279f2b
tan nguyen <bivani2805@gmail.com>
2025-01-13 11:42:36 +0000

    Finish typescript training

--- 02-variables/sample-type.js
+++ 02-variables/sample-type.js
@@ -2,6 +2,11 @@ var found = true;
 var grade = 88.6;
 var firstName = "Anup";
 var lastName = 'Kumar';
+// found = 0;
+// grade = "A";
+// firstName = false;
 console.log(found);
 console.log("The grade is " + grade);
 console.log("Hi " + firstName + " " + lastName);
+// use template String
+console.log("Hi ".concat(firstName, " ").concat(lastName));
